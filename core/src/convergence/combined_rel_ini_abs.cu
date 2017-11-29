7a8226239b6be5e57849cfd3d75607d2000e9b1d
marsaev <marsaev@nvidia.com>
2017-11-29 23:40:49 +0300

    Removed header duplicate

--- core/src/convergence/combined_rel_ini_abs.cu
+++ core/src/convergence/combined_rel_ini_abs.cu
@@ -25,7 +25,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <algorithm> // std::max
 #include "amg_config.h"
 #include "convergence/combined_rel_ini_abs.h"
 

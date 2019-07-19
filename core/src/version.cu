f6d26f61c220136ca839a7c0f7f29840edb555f1
marsaev <32493426+marsaev@users.noreply.github.com>
2019-07-19 19:16:58 +0300

    Delete autogenerated version.cu file

--- core/src/version.cu
+++
@@ -1,6 +0,0 @@
-#include <version.h>
-namespace amgx{
-const char __AMGX_BUILD_DATE__ [] = __DATE__;
-const char __AMGX_BUILD_TIME__ [] = __TIME__;
-const char __AMGX_BUILD_ID__ [] = "2.0.0.130-opensource";
-}

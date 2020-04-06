3da49a29daa9fb9991f2ccdb3c728e75eb4303e1
Matthew Martineau <mmartineau@nvidia.com>
2020-04-06 04:40:51 -0700

    Altered the Vector class to use pinned host allocations.

--- base/include/vector_thrust_allocator.h
+++ base/include/vector_thrust_allocator.h
@@ -148,7 +148,116 @@ class thrust_amgx_allocator
         inline bool operator!=(thrust_amgx_allocator const &a) {return !operator==(a); }
 }; // end thrust_amgx_allocator
 
+template<typename T>
+class thrust_amgx_pinned_allocator
+{
+    public:
+        /*! Type of element allocated, \c T. */
+        typedef T                                   value_type;
+
+        /*! Pointer to allocation. */
+        typedef T*               pointer;
+
+        /*! \c const pointer to allocation. */
+        typedef const T*         const_pointer;
+
+        /*! Reference to allocated element. */
+        typedef T&         reference;
+
+        /*! \c const reference to allocated element. */
+        typedef const T&   const_reference;
+
+        /*! Type of allocation size, \c std::size_t. */
+        typedef std::size_t    size_type;
+
+        /*! Type of allocation difference, \c pointer::difference_type. */
+        typedef std::ptrdiff_t difference_type;
+
+        /*! The \p rebind metafunction provides the type of a \p thrust_amgx_pinned_allocator
+         *  instantiated with another type.
+         *
+         *  \tparam U The other type to use for instantiation.
+         */
+        template<typename U>
+        struct rebind
+        {
+            /*! The typedef \p other gives the type of the rebound \p thrust_amgx_pinned_allocator.
+             */
+            typedef thrust_amgx_pinned_allocator<U> other;
+        }; // end rebind
+
+        /*! No-argument constructor has no effect. */
+        __host__ __device__
+        inline thrust_amgx_pinned_allocator() {}
+
+        /*! No-argument destructor has no effect. */
+        __host__ __device__
+        inline ~thrust_amgx_pinned_allocator() {}
+
+        /*! Copy constructor has no effect. */
+        __host__ __device__
+        inline thrust_amgx_pinned_allocator(thrust_amgx_pinned_allocator const &) {}
+
+        /*! Constructor from other \p thrust_amgx_pinned_allocator has no effect. */
+        template<typename U>
+        __host__ __device__
+        inline thrust_amgx_pinned_allocator(thrust_amgx_pinned_allocator<U> const &) {}
+
+        /*! Returns the address of an allocated object.
+         *  \return <tt>&r</tt>.
+         */
+        __host__ __device__
+        inline pointer address(reference r) { return &r; }
+
+        /*! Returns the address an allocated object.
+         *  \return <tt>&r</tt>.
+         */
+        __host__ __device__
+        inline const_pointer address(const_reference r) { return &r; }
+
+        /*! Allocates storage for \p cnt objects.
+         *  \param cnt The number of objects to allocate.
+         *  \return A \p pointer to uninitialized storage for \p cnt objects.
+         *  \note Memory allocated by this function must be deallocated with \p deallocate.
+         */
+        __host__
+        inline pointer allocate(size_type cnt,
+                                const_pointer = const_pointer(static_cast<T *>(0)))
+        {
+            void *ptr;
+            amgx::memory::cudaMallocHost(&ptr, sizeof(T)*cnt);
+            return pointer((T *)ptr);
+        } // end allocate()
+
+        /*! Deallocates storage for objects allocated with \p allocate.
+         *  \param p A \p pointer to the storage to deallocate.
+         *  \param cnt The size of the previous allocation.
+         *  \note Memory deallocated by this function must previously have been
+         *        allocated with \p allocate.
+         */
+        __host__
+        inline void deallocate(pointer p, size_type cnt)
+        {
+            amgx::memory::cudaFreeHost((void *)p);
+        } // end deallocate()
+
+        /*! Compares against another \p thrust_amgx_pinned_allocator for equality.
+         *  \return \c true
+         */
+        __host__ __device__
+        inline bool operator==(thrust_amgx_pinned_allocator const &) { return true; }
+
+        /*! Compares against another \p thrust_amgx_pinned_allocator for inequality.
+         *  \return \c false
+         */
+        __host__ __device__
+        inline bool operator!=(thrust_amgx_pinned_allocator const &a) {return !operator==(a); }
+}; // end thrust_amgx_pinned_allocator
+
 template<class T>
 using device_vector_alloc = thrust::device_vector<T, thrust_amgx_allocator<T>>;
+
+template<class T>
+using pinned_vector_alloc = thrust::host_vector<T, thrust_amgx_pinned_allocator<T>>;
 
 } // end amgx

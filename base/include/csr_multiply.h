c12566cbfe8428739653c1c6ce79867a2ceeb820
Marcin Rogowski <mrogowski@nvidia.com>
2020-08-13 06:46:30 -0700

    Minor corrections to previous CUDA 11 compatibility fixes

--- base/include/csr_multiply.h
+++ base/include/csr_multiply.h
@@ -156,11 +156,6 @@ class CSR_Multiply_Impl<TemplateConfig<AMGX_device, V, M, I> > : public Hash_Wor
 
         // Compute the product between two CSR matrices.
         void multiply( const Matrix_d &A, const Matrix_d &B, Matrix_d &C, IVector *Aq1, IVector *Bq1, IVector *Aq2, IVector *Bq2 );
-#ifdef CUSPARSE_USE_SPGEMM
-        void cusparse_multiply_generic( const Matrix_d &A, const Matrix_d &B, Matrix_d &C, IVector *Aq1, IVector *Bq1, IVector *Aq2, IVector *Bq2 );
-#else
-        void cusparse_multiply( const Matrix_d &A, const Matrix_d &B, Matrix_d &C, IVector *Aq1, IVector *Bq1, IVector *Aq2, IVector *Bq2 );
-#endif
 
 
         void sparse_add( Matrix_d &RAP, const Matrix_d &RAP_int, std::vector<IVector> &RAP_ext_row_offsets, std::vector<IVector> &RAP_ext_col_indices, std::vector<MVector> &RAP_ext_values, std::vector<IVector> &RAP_ext_row_ids);
@@ -198,6 +193,13 @@ class CSR_Multiply_Impl<TemplateConfig<AMGX_device, V, M, I> > : public Hash_Wor
         // Compute values.
         virtual void compute_values( const Matrix_d &A, const Matrix_d &B, Matrix_d &C, int num_threads, IVector *Aq1, IVector *Bq1, IVector *Aq2, IVector *Bq2 ) = 0;
         virtual void compute_values_RAP_sparse_add( Matrix_d &RAP, const Matrix_d &RAP_int, std::vector<IVector> &RAP_ext_row_offsets, std::vector<IVector> &RAP_ext_col_indices, std::vector<MVector> &RAP_ext_values, std::vector<IVector> &RAP_ext_row_ids, int num_threads) = 0;
+
+    private:
+#ifdef CUSPARSE_USE_GENERIC_SPGEMM
+        void cusparse_multiply_generic( const Matrix_d &A, const Matrix_d &B, Matrix_d &C, IVector *Aq1, IVector *Bq1, IVector *Aq2, IVector *Bq2 );
+#else
+        void cusparse_multiply( const Matrix_d &A, const Matrix_d &B, Matrix_d &C, IVector *Aq1, IVector *Bq1, IVector *Aq2, IVector *Bq2 );
+#endif
 
 };
 

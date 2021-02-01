b8e5f3a1a41249392485d7f30f77e9f8309a3976
Matt Martineau <mmartineau@nvidia.com>
2021-02-01 01:33:44 -0800

    Fix for GMRES convergence bug.

--- core/src/solvers/gmres_solver.cu
+++ core/src/solvers/gmres_solver.cu
@@ -393,13 +393,13 @@ GMRES_Solver<T_Config>::solve_iteration( VVector &b, VVector &x, bool xIsZero )
         // Call the preconditioner to get M^-1*(sum_m vm*ym), store in m_V_Vectors[0]
         if (no_preconditioner)
         {
-            copy( m_V_vectors[0], m_Z_vector, offset, size);
+            copy( m_Z_vector, m_V_vectors[0], offset, size);
         }
         else
         {
             m_V_vectors[0].delayed_send = 1;
             m_Z_vector.delayed_send = 1;
-            m_preconditioner->solve( m_V_vectors[0], m_Z_vector, true );
+            m_preconditioner->solve( m_Z_vector, m_V_vectors[0], true );
             m_V_vectors[0].delayed_send = 1;
             m_Z_vector.delayed_send = 1;
         }

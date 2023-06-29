3d355797ec6381669cfa14afce4985f9febb9eb5
Matt Martineau <mmartineau@nvidia.com>
2023-06-29 09:15:16 +0000

    Fixing missed merge conflict.

--- include/generic_spmv_csr.h
+++ include/generic_spmv_csr.h
@@ -288,13 +288,8 @@ void retrieveTwoArguments(const inVector &in,
                           Vector<TemplateConfig<AMGX_device, t_vecPrecTwo, t_matPrec, t_indPrec> > &out2)
 {
     const int blockSize = 128;
-<<<<<<< HEAD
-    const int numBlocks = min( (int)(AMGX_GRID_MAX_SIZE), (int)(in.size() / blockSize + 1));
-    retrieveTwoKernel <<< numBlocks, blockSize>>>(amgx::thrust::raw_pointer_cast(&in[0]),
-=======
     const int numBlocks = std::min( (int)(AMGX_GRID_MAX_SIZE), (int)(in.size() / blockSize + 1));
-    retrieveTwoKernel <<< numBlocks, blockSize>>>(thrust::raw_pointer_cast(&in[0]),
->>>>>>> main
+    retrieveTwoKernel <<< numBlocks, blockSize>>>(amgx::thrust::raw_pointer_cast(&in[0]),
             out1.raw(),
             out2.raw(),
             (int)in.size());
@@ -331,13 +326,8 @@ void retrieveOneArgument(const inVector &in,
                          Vector<TemplateConfig<AMGX_device, t_vecPrec, t_matPrec, t_indPrec> > &out)
 {
     const int blockSize = 128;
-<<<<<<< HEAD
-    const int numBlocks = min( (int)(AMGX_GRID_MAX_SIZE), (int)(in.size() / blockSize + 1));
-    retrieveOneKernel <<< numBlocks, blockSize>>>(amgx::thrust::raw_pointer_cast(&in[0]),
-=======
     const int numBlocks = std::min( (int)(AMGX_GRID_MAX_SIZE), (int)(in.size() / blockSize + 1));
-    retrieveOneKernel <<< numBlocks, blockSize>>>(thrust::raw_pointer_cast(&in[0]),
->>>>>>> main
+    retrieveOneKernel <<< numBlocks, blockSize>>>(amgx::thrust::raw_pointer_cast(&in[0]),
             out.raw(),
             (int)in.size());
     cudaCheckError();
